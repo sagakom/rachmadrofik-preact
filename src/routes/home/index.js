35bdde65e50fe024a03e9f9697bcd12a8c5210d0
SAMITRA DEV <saga.komputer@gmail.com>
2020-03-04 22:17:24 +0700

    Update index.js

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -36,16 +36,16 @@ const Home = () => {
 					“Design is the silent ambassador of your brand.” (Paul Rand)
 					“Mass advertising can help build brands, but authenticity is what makes them last. If people believe they share values with a company, they will stay loyal to the brand.” (Howard Schultz)
 					“A brand for a company is like a reputation for a person. You earn reputation by trying to do hard things well.” (Jeff Bezos)
+					“Your brand is what other people say about you when you’re not in the room.” (Jeff Bezos)
 
 				</p>
 				<p class={style.bioright}>
-					Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-					accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae
-					ab illo inventore veritatis et quasi architecto beatae vitae dicta
-					sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
-					aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos
-					qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui
-					dolorem ipsum quia dolor sit amet, consectetur.
+					“Brand is just a perception, and perception will match reality over time. Sometimes it will be ahead, other times it will be behind. But brand is simply a collective impression some have about a product.” (Elon Musk)
+					“A brand is the set of expectations, memories, stories and relationships that, taken together, account for a consumer’s decision to choose one product or service over another.” (Seth Godin)
+					“Every advertisement should be thought of as a contribution to the complex symbol which is the brand image.” (David Ogilvy)
+					“Too many companies want their brands to reflect some idealised, perfected image of themselves. As a consequence, their brands acquire no texture, no character and no public trust.” (Richard Branson)
+					“Your brand is what other people say about you when you’re not in the room.” (Jeff Bezos)
+
 				</p>
 			</div>
 		</div>
